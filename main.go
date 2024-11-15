adf0843679ea9cdd267d348cf0de2b25e80e3345
Yodahe Teshome <jodahe2001@gmail.com>
2024-11-15 18:09:19 +0300

    dream route created

--- main.go
+++ main.go
@@ -33,10 +33,30 @@ func hobbyHandler(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusMethodNotAllowed)
 	}
 }
+
+
+
+type Dream struct {
+	Message string `json:"message"`
+}
+
+
+func dreamHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		dream := Dream{
+			Message: "I aim to solve real-world problems, and create innovative solutions that positively impact communities and businesses",
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(dream)
+	} else {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+	}
+}
 func main() {
 	http.HandleFunc("/name", nameHandler)
 	http.HandleFunc("/hobby", hobbyHandler)
-	
+	http.HandleFunc("/dream", dreamHandler)
 
 	fmt.Println("Server is starting on :8080...")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
