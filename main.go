21667a84c6d8e28aa52363d82df76ee11291ab0c
Yodahe Teshome <jodahe2001@gmail.com>
2024-11-15 17:54:05 +0300

    /hobby Route  created wich displaymy 2 hobbies

--- main.go
+++ main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
-
 
 func nameHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodGet {
@@ -15,9 +15,29 @@ func nameHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+
+type Hobby struct {
+	FavoriteHobby string `json:"favorite_hobby"`
+}
+
+
+func hobbyHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		hobby := Hobby{
+			FavoriteHobby: "Playing and Watching Football , Reading Books", 
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(hobby)
+	} else {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+	}
+}
 func main() {
 	http.HandleFunc("/name", nameHandler)
+	http.HandleFunc("/hobby", hobbyHandler)
 	
+
 	fmt.Println("Server is starting on :8080...")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		fmt.Println("Error starting server:", err)
