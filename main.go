033d64345538c02c26c3720278aee3abeeedcd1c
Yodahe Teshome <jodahe2001@gmail.com>
2024-11-15 17:48:34 +0300

    /name Route created

--- main.go
+++ main.go
@@ -5,11 +5,19 @@ import (
 	"net/http"
 )
 
+
+func nameHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Yodahe Teshome")) 
+	} else {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+	}
+}
+
 func main() {
 	http.HandleFunc("/name", nameHandler)
-	http.HandleFunc("/hobby", hobbyHandler)
-	http.HandleFunc("/dream", dreamHandler)
-
+	
 	fmt.Println("Server is starting on :8080...")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		fmt.Println("Error starting server:", err)
